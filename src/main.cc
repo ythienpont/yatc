331776b2e059d44ff42973ac83679629ba346f9d
ythienpont <yonah.thienpont@skynet.be>
2024-03-11 23:21:36 +0100

    Updated README

--- src/main.cc
+++ src/main.cc
@@ -1,22 +1,22 @@
 #include "TrackerClient/TrackerClient.h"
 
-std::array<char, 20> hexStringToByteArray(const std::string& hex) {
-    if (hex.length() != 40) {
-        throw std::invalid_argument("Invalid input length for an info hash");
-    }
+std::array<char, 20> hexStringToByteArray(const std::string &hex) {
+  if (hex.length() != 40) {
+    throw std::invalid_argument("Invalid input length for an info hash");
+  }
 
-    std::array<char, 20> bytes;
-    for (size_t i = 0, j = 0; i < hex.length(); i += 2, ++j) {
-        std::string byteString = hex.substr(i, 2);
-        char byte = static_cast<char>(std::stoi(byteString, nullptr, 16));
-        bytes[j] = byte;
-    }
-    return bytes;
+  std::array<char, 20> bytes;
+  for (size_t i = 0, j = 0; i < hex.length(); i += 2, ++j) {
+    std::string byteString = hex.substr(i, 2);
+    char byte = static_cast<char>(std::stoi(byteString, nullptr, 16));
+    bytes[j] = byte;
+  }
+  return bytes;
 }
 
-
 int main(int argc, char *argv[]) {
-  TrackerClient tc("https://ipv6.torrent.ubuntu.com/announce",
+  TrackerClient tc(
+      "https://ipv6.torrent.ubuntu.com/announce",
       hexStringToByteArray("9ecd4676fd0f0474151a4b74a5958f42639cebdf"),
       5173995520);
 
